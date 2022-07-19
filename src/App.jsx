b5d4f7d8e0c98c6dfc95270da50f61976250e111
vero <veronica.delacruz12@hotmail.com>
2022-07-19 02:26:51 -0500

    Feat: Add file to assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-
+import Nav from './components/navbar'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <Nav/>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
